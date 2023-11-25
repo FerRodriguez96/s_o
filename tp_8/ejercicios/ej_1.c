042f753a9ca7312595116441bc4b9addb8a8c6c0
FerRodriguez96 <feer.rodriguez96@gmail.com>
2023-11-25 11:08:53 -0300

    correccion ejercicio 1

--- tp_8/ejercicios/ej_1.c
+++ tp_8/ejercicios/ej_1.c
@@ -8,6 +8,8 @@
 #include <dirent.h>
 // libreria para obtener informacion de directorios
 #include <sys/stat.h>
+// libreria para tener constantes de limites de sistema
+#include <linux/limits.h>
 
 /*Escriba un programa que recibe un parámetro obligatorio y dos
 parámetros opcionales. El parámetro obligatorio es una palabra y los opcionales son un
@@ -24,86 +26,118 @@ regulares o directorios que contengan la palabra pasada como parámetro al progr
 
 void imprimir_contenido(char *ruta, char *palabra, int bandera_eliminacion)
 {
+    // declaracion de estructuras
     DIR *dirp;
     struct dirent *direntp;
     struct stat atributos;
 
-    dirp = opendir(ruta);
+    // declaracion de variables
+    long long cant_reg = 0, cant_dir = 0, cant_otr = 0;
 
+    // apertura de directorio
+    dirp = opendir(".");
+
+    // comprobacion de apertura correcta
     if (dirp == NULL)
     {
         perror("Error al abrir el directorio");
         exit(1);
     }
 
-    printf("Directorio actual: %s\n", ruta);
-
+    // lectura de directorio
     while ((direntp = readdir(dirp)) != NULL)
     {
-        char file_path[1024];
-
-        printf(file_path, sizeof(file_path), "%s/%s", ruta, direntp->d_name);
-
-        if (stat(file_path, &atributos) == 0)
+        // busqueda de la palabra dentro de los nombres del directorio
+        if (strstr(direntp->d_name, palabra) != NULL)
         {
-            if (S_ISREG(atributos.st_mode))
-            {
-                printf("Archivo regular: %s\n", direntp->d_name);
-            }
-            else if (S_ISDIR(atributos.st_mode))
-            {
-                printf("Directorio: %s\n", direntp->d_name);
-            }
-            else
-            {
-                printf("Otro archivo:%s\n", direntp->d_name);
-            }
+            printf("Contiene la palabra: %s\n", direntp->d_name);
 
-            if (strstr(direntp->d_name, palabra) != NULL)
+            // obtencion de atributos de directorio
+            if (stat(direntp->d_name, &atributos) == 0)
             {
-                printf("Contiene la palabra: %s\n", direntp->d_name);
-
-                if (bandera_eliminacion == 1 && (S_ISREG(atributos.st_mode) || S_ISDIR(atributos.st_mode)))
+                if (S_ISREG(atributos.st_mode) != 0)
                 {
-                    remove(file_path);
-                    printf("Archivo eliminado: %s\n", direntp->d_name);
+                    // suma de bytes de archivos regulares
+                    cant_reg += atributos.st_size;
+                }
+                else if (S_ISDIR(atributos.st_mode) != 0)
+                {
+                    // suma de bytes de directorios
+                    cant_dir += atributos.st_size;
+                }
+                else
+                {
+                    printf("%s es otro archivo\n", direntp->d_name);
+
+                    // suma de bytes de otros archivos
+                    cant_otr += atributos.st_size;
+
+                    // eliminacion de otros archivos si se ingreso el parametro "e"
+                    if (bandera_eliminacion == 1)
+                    {
+                        if (unlink(direntp->d_name) == -1)
+                        {
+                            perror("Error al eliminar el archivo");
+                            exit(1);
+                        }
+                        else
+                        {
+                            printf("Se ha eliminado el archivo: %s\n", direntp->d_name);
+                        }
+                    }
                 }
             }
         }
     }
 
+    // impresion de valores de bytes
+    printf("La cantidad de bytes que ocupan los archivos regulares es de %lld\n", cant_reg);
+    printf("La cantidad de bytes que ocupan los directorios es de %lld\n", cant_dir);
+    printf("La cantidad de bytes que ocupan otros archivos es de %lld\n", cant_otr);
+
+    // cierre de directorio
     closedir(dirp);
 }
 
 int main(int argc, char *argv[])
 {
-    if (argc <= 2 || argc > 4)
+    // comprobacion de numero de argumentos
+    if (argc < 2 || argc > 4)
     {
-        printf("Error en la cantidad de argumentos ingresados");
+        printf("Error en la cantidad de argumentos ingresados\n");
         exit(1);
     }
 
+    // asigna la palabra al primer argumento
     char *palabra = argv[1];
-    /*primera idea?
-    char * directorio = argv[2];
-    */
 
-    /*
-     operador ternario
-     Si el numero de argumentos pasado es mayor a 3
-     (esto include a la letra e) entonces el valor de
-     directorio sera argv[2], si no, directorio sera
-     "." que es el valor del directorio actual
-    */
-    char *directorio = (argc >= 3) ? argv[2] : ".";
+    // declaracion de buffer
+    char buf[PATH_MAX];
 
-    // Si se proporciona un directorio, cambia ahi:
-    if (chdir(directorio) != 0)
+    // se obtiene el directorio actual
+    if (getcwd(buf, PATH_MAX) == NULL)
     {
-        perror("Error al cambiar de directorio");
+        perror("getcwd");
         exit(1);
     }
+    else
+    {
+        printf("Directorio actual: %s\n", buf);
+    }
 
+    printf("Cambiando directorio...\n");
+
+    if (argc >= 3)
+    {
+        // cambio de directorio
+        if (chdir(argv[2]) == -1)
+        {
+            perror("chdir");
+            exit(1);
+        }
+
+        printf("Nuevo directorio: %s\n", argv[2]);
+    }
     /*
         En primer lugar, se verifica que se tienen 4 argumentos
         y en segundo, si el argumento 4 es igual a "e".
@@ -113,14 +147,16 @@ int main(int argc, char *argv[])
         En caso de que ambas condiciones se cumplan, la bandera
         de eliminar la palabra pasa de 0 a 1.
     */
+
     int bandera_eliminar = 0;
+
     if (argc == 4 && strcmp("e", argv[3]) == 0)
     {
         bandera_eliminar = 1;
     }
 
     // llamado de funcion
-    imprimir_contenido(".", palabra, bandera_eliminar);
+    imprimir_contenido(buf, palabra, bandera_eliminar);
 
     return 0;
 }
